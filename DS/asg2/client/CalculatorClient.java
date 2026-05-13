8aa8b498d1b1e905ed3439c9e65508d8ef807852
Asutosh panda <122630330+asutoshpanda1611@users.noreply.github.com>
2026-05-13 21:11:20 +0530

    Update CalculatorClient.java

--- DS/asg2/client/CalculatorClient.java
+++ DS/asg2/client/CalculatorClient.java
@@ -11,26 +11,16 @@ public class CalculatorClient {
 
    public static void main(String args[]) {
        try {
+            ORB orb = ORB.init(args, null);
 
-           // create and initialize the ORB
-           ORB orb = ORB.init(args, null);
+            NamingContextExt nc = NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
+            Calculator obj = CalculatorHelper.narrow(nc.resolve_str("Calculator"));
 
-           // get the root naming context
-           // NameService invokes the transient name service
-           org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-           // Use NamingContextExt which is part of the Interoperable
-           // Naming Service (INS) specification.
-           NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            System.out.println(obj.add(10, 5));
+            System.out.println(obj.subtract(10, 5));
+            System.out.println(obj.multiply(10, 5));
+            System.out.println(obj.divide(10, 5));
 
-           // resolve the Object Reference in Naming
-           String name = "Calculator";
-           Calculator calculator = CalculatorHelper.narrow(ncRef.resolve_str(name));
-
-           System.out.println("Obtained a handle on server object");
-           System.out.println(calculator.add(1, 2));
-           System.out.println(calculator.subtract(1, 2));
-           System.out.println(calculator.multiply(1, 2));
-           System.out.println(calculator.divide(1, 2));
 
        } catch (Exception e) {
            System.out.println("ERROR : " + e);
