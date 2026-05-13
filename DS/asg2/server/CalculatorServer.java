3d7ef3bc2b3c1b5e4f13901131dda98c95d14674
Asutosh panda <122630330+asutoshpanda1611@users.noreply.github.com>
2026-05-13 21:10:53 +0530

    Update CalculatorServer.java

--- DS/asg2/server/CalculatorServer.java
+++ DS/asg2/server/CalculatorServer.java
@@ -11,25 +11,19 @@ import calculator_module.CalculatorHelper;
 public class CalculatorServer {
    public static void main(String args[]){
        try {
-           ORB orb = ORB.init(args, null);
-           POA rootpoa = (POA)orb.resolve_initial_references("RootPOA");
-           rootpoa.the_POAManager().activate();
+            ORB orb = ORB.init(args, null);
+            POA root = (POA) orb.resolve_initial_references("RootPOA");
+            root.the_POAManager().activate();
 
-           CalculatorImpl calculatorImpl = new CalculatorImpl();
-           calculatorImpl.setORB(orb);
+            CalculatorImpl obj = new CalculatorImpl();
+            obj.setORB(orb);
 
-           org.omg.CORBA.Object ref = rootpoa.servant_to_reference(calculatorImpl);
-           Calculator href = CalculatorHelper.narrow(ref);
+            Calculator ref = CalculatorHelper.narrow(root.servant_to_reference(obj));
+            NamingContextExt nc = NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
 
-           org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");        
-           NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-
-           String name = "Calculator";
-           NameComponent path[] = ncRef.to_name( name );
-           ncRef.rebind(path, href);
-
-           System.out.println("CalculatorServer ready and waiting ...");
-           orb.run();
+            nc.rebind(nc.to_name("Calculator"), ref);
+            System.out.println("Server Ready...");
+            orb.run();
 
        } catch (Exception e) {
            System.err.println("ERROR: " + e);
